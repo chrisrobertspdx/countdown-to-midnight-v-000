bdd9b4c06d56bd61b44e3c7be0d91a0aecc85feb
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-23 16:35:57 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,4 +7,10 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-
+def countdown_with_sleep(number)
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
